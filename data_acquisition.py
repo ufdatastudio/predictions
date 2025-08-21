d888b9d23a1e84e641fa2ad30421486cafde7f12
Detravious Jamari Brinkley <dj.brinkley@ufl.edu>
2025-08-21 10:59:48 -0400

    feat: collecting Open Measures data using builder pattern
    1. https://docs.openmeasures.io/docs/guides/public-api
    2. https://refactoring.guru/design-patterns/builder

---
+++ data_acquisition.py
@@ -0,0 +1,77 @@
+"""Detravious Jamari Brinkley, Kingdom Man
+
+Dr. Grant @ UF Data Studio
+
+
+"""
+import requests
+
+from abc import abstractmethod
+
+class OpenMeasuresBuilder:
+    """A class to execute the building process. Each function is like a section (0341s, 0311s) that have a specific focus in the process.
+    
+    Process follows notebook: https://colab.research.google.com/drive/1kDyRIC0NBOj4Egn_VdK837QBNqDERRi_?usp=sharing
+    """
+
+    @abstractmethod
+    def reset(self):
+        pass
+
+    @abstractmethod
+    def set_terms(self, terms):
+        # type is dict
+        if isinstance(terms, dict):
+            pass
+        
+        elif isinstance(terms, str):
+            self.terms_for_query = terms
+            # self.terms_for_query = 
+
+        # type is list
+
+
+
+    @abstractmethod
+    def set_query(self, limit: int, site: str, start_date: str, end_date: str, querytype: str):
+        """Required parameters to query Open Measure"""
+
+        self.params = {
+            'term' : self.terms_for_query,
+            'limit': limit,
+            'site': site,
+            'since': start_date,
+            'until': end_date,
+            'querytype': querytype
+            }
+        
+        # we can create a URL to represent this query
+        self.url = 'http://api.smat-app.com/content?{}'.format(
+            '&'.join(
+                [f"{k}={v}" for k,v in self.params.items()]
+            )
+        )
+
+        print(f"Query's URL: {self.url}")
+                
+    @abstractmethod
+    def get_raw_hits(self):
+        r = requests.get(self.url)
+        r.status_code
+        data = r.json()
+        hits = data['hits']['hits']
+        # all_hits = hits[0]['_source']
+
+        return hits
+
+class OpenMeasuresDirector:
+    """A class to orchestrate the building process. Think at large/overview/blue print or this is like the CO laying the plan to the entire company. The execution (Builder) has functions and these functions are different sections (0341s, 0311s, etc) acting."""
+    def construct_from_dataset(builder: OpenMeasuresBuilder, terms, limit: int, site: str, start_date: str, end_date: str, querytype: bool):
+        """Construct all datasets"""
+
+        builder.reset()
+        builder.set_terms(terms)
+        builder.set_query(limit, site, start_date, end_date, querytype)
+        return builder.get_raw_hits()
+
+
