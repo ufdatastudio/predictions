bed7ab49d5f71ea6a3ce3dffdc13a85598e24ddb
brinkley97 <dj.brinkley@ufl.edu>
2025-10-01 13:37:57 -0400

    feat: load and query vector store
    + Only for Chroma
    
    + Created a mixin class, BaseVectorStoreLoader class, and query() function within the VectorStoreDirector class
    
    + Need to test storing data again as I updated ChromaVectorStore to have both BaseVectorStoreBuilder, BaseVectorStoreLoader

--- vector_stores.py
+++ vector_stores.py
@@ -28,16 +28,7 @@ from langchain_core.documents import Document
 from langchain_community.vectorstores import FAISS
 from langchain_community.docstore.in_memory import InMemoryDocstore
 
-class BaseVectorStoreBuilder(ABC):
-    def __init__(self):
-        self.vector_store = None
-        self.documents = []
-        self.uuids = None
-        self.embedding_model = None
-    
-    def reset(self):
-        self.__init__() # Resets all properties to their initial state
-        print(f"\tVector Store: {self.vector_store}\n\tDocments: {self.documents}\n\tUUIDS: {self.uuids}\n\tEmbedding Model: {self.embedding_model}")
+class BaseVectorStoreMixin(ABC):
     
     def set_embedding_model(self, embedding_model_name):
         supported_models = {
@@ -56,9 +47,18 @@ class BaseVectorStoreBuilder(ABC):
                 Embedding Model Name: '{embedding_model_name}' is not yet supported.
                 Choose from: {supported_models}
             """)
+
+class BaseVectorStoreBuilder(BaseVectorStoreMixin):
+    def __init__(self):
+        self.vector_store = None
+        self.documents = []
+        self.uuids = None
+        self.embedding_model = None
     
-    
-    # @abstractmethod
+    def reset(self):
+        self.__init__() # Resets all properties to their initial state
+        print(f"\tVector Store: {self.vector_store}\n\tDocments: {self.documents}\n\tUUIDS: {self.uuids}\n\tEmbedding Model: {self.embedding_model}")
+            
     def build_documents(self, df: pd.DataFrame):
         """(Abstract) Processes raw data into LangChain Documents."""
         metadata_cols = [col for col in df.columns if col != 'sentence']
@@ -91,16 +91,34 @@ class BaseVectorStoreBuilder(ABC):
         if not self.vector_store:
             raise ValueError("Product has not been built yet.")
         return self.vector_store
-    
         
     @abstractmethod
     def initialize_vector_store(self):
         """(Abstract) Creates the initial vector store object."""
         pass
 
-class ChromaVectorStore(BaseVectorStoreBuilder):
+    @abstractmethod
+    def load_vector_store(self):
+        pass
     
-    def __init__(self, collection_name: str = None, persist_directory: str = None):
+
+class BaseVectorStoreLoader(BaseVectorStoreMixin):
+    def __init__(self):
+        self.client = None
+    
+    def reset(self):
+        self.__init__() # Resets all properties to their initial state
+    
+    def load_vector_store(self):
+        self.client = client
+    
+class ChromaVectorStore(BaseVectorStoreBuilder, BaseVectorStoreLoader):
+    
+    def __init__(self,
+                 collection_name: str = None,
+                 persist_directory: str = None
+                ):
+        self.client = None
         self.collection_name = collection_name
         self.persist_directory = persist_directory
         self.vector_store = None
@@ -122,12 +140,55 @@ class ChromaVectorStore(BaseVectorStoreBuilder):
         )
         print(f"\tVector Store (Original): {self.vector_store}")
         
+    def load_vector_store(self, client_type: str = None):
+        if client_type == 'Persistence':
+            self.client = chromadb.PersistentClient(path=self.persist_directory)
+            if not self.embedding_model:
+                raise ValueError("Embedding model must be set before initializing the vector store.")
+        
+            print(f"\tCollection Name: {self.collection_name}\n\tEmbedding Model: {self.embedding_model}\n\tPersist Directory: {self.persist_directory}")
+            
+            self.vector_store = Chroma(
+                client=self.client,
+                collection_name=self.collection_name,
+                embedding_function=self.embedding_model,
+            )
+            print(f"\tVector Store (Original): {self.vector_store}")
+    
+    def query_vector_store(self, query_string, k):
+        print("\t1. Similarity")
+        results = self.vector_store.similarity_search(query_string, k=k)
+        for res in results:
+            print(f"\t\t* {res.page_content} [{res.metadata}]\n")
+
+        print("\t2. Similarity with score")
+        results = self.vector_store.similarity_search_with_score(query_string, k=k)
+        for res, score in results:
+            print(f"\t\t* [SIM={score:3f}] {res.page_content} [{res.metadata}]\n")
+        
+        print("\t3. Similarity by vector")
+        results = self.vector_store.similarity_search_by_vector(embedding=self.embedding_model.embed_query(query_string), k=k)
+        for doc in results:
+            print(f"\t\t* {doc.page_content} [{doc.metadata}]\n")
+    
+        print("\t4. Retriever")
+        retriever = self.vector_store.as_retriever(search_type="mmr", search_kwargs={"k": k, "fetch_k": k})
+        retriever.invoke(query_string)
+        print(f"\t\t* {retriever}\n")
+        
+
 class VectorStoreDirector:
-    def __init__(self, builder: BaseVectorStoreBuilder):
+    def __init__(self, builder: BaseVectorStoreBuilder = None, loader: BaseVectorStoreLoader = None):
         """Initializes the director with a specific builder instance."""
-        print(f"### BUILDER ###")
-        self._builder = builder
-        print(f"\t{self._builder}")
+        if builder:
+            print(f"### BUILDER ###")
+            self._builder = builder
+            print(f"\t{self._builder}")
+        
+        if loader: 
+            print(f"### LOADER ###")
+            self._loader = loader
+            print(f"\t{self._loader}")
     
     def construct(self, embedding_model_name, data):
 
@@ -150,7 +211,25 @@ class VectorStoreDirector:
         self._builder.add_documents_to_vector_store()
         print(f"\tDocuments added: {self._builder.vector_store}")
     
-    def get_vector_store(self):
-        """Retrieves the final product from the builder."""
-        return self._builder.get_vector_store()
+    def query(self, embedding_model_name, query_string, k):
+        """Retrieves the final vector store from the builder."""
+        
+        print(f"### INITIALIZE CLIENT VECTOR STORE ###")
+        self._loader.load_vector_store()
+        print(f"\tVector Store (Prediction's Wrapper): {self._loader.client}")
+        
+        print(f"### LOAD EMBEDDING MODEL ###")
+        self._loader.set_embedding_model(embedding_model_name)
+        print(f"\t{embedding_model_name}")
+        
+        print(f"### LOAD VECTOR STORE ###")
+        self._loader.initialize_vector_store()
+        print(f"\tVector Store (Prediction's Wrapper): {self._loader}")
+        
+        print(f"### TOP K ###")
+        results = self._loader.query_vector_store(query_string, k)       
+        print(f"\tQuery Results): {results}")
+
+        
+
     
