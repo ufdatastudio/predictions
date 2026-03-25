0ec61c4a518e9175761802c58d33f77604ef2971
brinkley97 <dj.brinkley@ufl.edu>
2026-03-25 13:55:44 -0400

    fix: duplicate files

--- bash_scripts/hpg_slurm_scripts/general/general-v1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-srun --account=ufdatastudios --nodes=1 --gpus=1 --time=03:00:00 --mem=64G --cpus-per-task=8 --partition=hpg-b200 --pty bash
--- bash_scripts/hpg_slurm_scripts/general/general.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-srun --account=ufdatastudios --nodes=1 --time=72:00:00 --mem=120G --cpus-per-task=32 --pty bash
