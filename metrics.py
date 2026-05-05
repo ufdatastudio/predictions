ce4789d5be182f02bd9271f379c9ab37914aa801
Detravious Jamari Brinkley <dj.brinkley@ufl.edu>
2026-05-05 08:11:01 -0400

    feat: cosine similarity

--- metrics.py
+++ metrics.py
@@ -173,16 +173,23 @@ class EvaluationMetric:
         return pd.DataFrame(cohens_kappa_scores)
 
 # EMBEDDINGS
-    def get_cosine_similarity(prediction_embeddings: np.array, observation_embeddings: np.array) -> list:
-        assert len(prediction_embeddings) == len(observation_embeddings)
+    def get_cosine_similarity(x_embeddings: np.array, y_embeddings: np.array, per_row: bool) -> list:
+        assert len(x_embeddings) == len(y_embeddings)
 
         model_scores = []
-        for i in tqdm(range(len(prediction_embeddings))):
+        if per_row:
+            for i in tqdm(range(len(x_embeddings))):
 
-            # make them (1 × vector_dim) for sklearn
-            pred_sent_embedding_reshaped = prediction_embeddings[i].reshape(1, -1)
-            obser_sent_embedding_reshaped = observation_embeddings[i].reshape(1, -1)
-            cos_sim = cosine_similarity(pred_sent_embedding_reshaped, obser_sent_embedding_reshaped)[0, 0]
-            model_scores.append(cos_sim)
+                # make them (1 × vector_dim) for sklearn
+                x_embeddings_reshaped = x_embeddings[i].reshape(1, -1)
+                y_embeddings_reshaped = y_embeddings[i].reshape(1, -1)
+                cos_sim = cosine_similarity(x_embeddings_reshaped, y_embeddings_reshaped)[0, 0]
+                model_scores.append(cos_sim)
+
+            return model_scores
+        else: 
+            cos_sim = cosine_similarity(x_embeddings, y_embeddings)
+            return cos_sim
+            # model_scores.append(cos_sim)
         
-        return model_scores
+        
